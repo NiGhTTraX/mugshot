8875a860589a60fb6706468c2467bdec12184210
Simion Florentin <florentin.simion@hootsuite.com>
2015-07-22 17:32:01 +0300

    adding comments in test file

--- test/mugshot.test.js
+++ test/mugshot.test.js
@@ -5,22 +5,30 @@ describe('Mugshot constructor', function() {
 
 	var dummyBrowser = 'anything';
 
-	it('shoould throw an error if no browser instance is provided', function() {
+	it('should throw an error if no browser instance is provided', function() {
 		expect(Mugshot).to.throw(Error);
 	});
 
 	it('should throw an error if options is not an object', function() {
 
+		// options is an Array
 		expect(Mugshot.bind({}, dummyBrowser, [])).to.throw(Error);
+
+		//options is a String
 		expect(Mugshot.bind({}, dummyBrowser, '')).to.throw(Error);
+		
+		// options is a Number
 		expect(Mugshot.bind({}, dummyBrowser, 3)).to.throw(Error);
+		
+		// options is a function	
 		expect(Mugshot.bind({}, dummyBrowser, function() {})).to.throw(Error);
+		
+		// options is undefined
 		expect(Mugshot.bind({}, dummyBrowser, undefined)).to.throw(Error);
 
 	});
 
 	it('should not throw an error if options is not provided', function() {
-
 		expect(Mugshot.bind({}, dummyBrowser)).to.not.throw(Error);
 	});
 
