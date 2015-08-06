7b48b1d56f13e1ca6d1c7bf40674d63bd5d227cd
Simion Florentin <florentin.simion@hootsuite.com>
2015-08-06 17:32:06 +0300

    update tests to use directly the buffer

--- test/differ-tests.js
+++ test/differ-tests.js
@@ -63,11 +63,10 @@ function differTests(getDifferInstance) {
       var img2 = composePath('different.png');
       var diff = composePath('strict.png');
 
-      differInstance.createDiff(img1, img2, function(error, base64) {
-        var buffer = new Buffer(base64, 'base64');
+      differInstance.createDiff(img1, img2, function(error, data) {
         expect(error).to.be.null;
 
-        differInstance.isEqual(diff, buffer, function(error, equal) {
+        differInstance.isEqual(diff, data, function(error, equal) {
           expect(error).to.be.null;
           expect(equal).to.be.equal(true);
           done();
@@ -81,11 +80,10 @@ function differTests(getDifferInstance) {
         var img2 = composePath('different-unnoticeable.png');
         var diff = composePath('strict-unnoticeable.png');
 
-        differInstance.createDiff(img1, img2, function(error, base64) {
-          var buffer = new Buffer(base64, 'base64');
+        differInstance.createDiff(img1, img2, function(error, data) {
           expect(error).to.be.null;
 
-          differInstance.isEqual(diff, buffer, function(error, equal) {
+          differInstance.isEqual(diff, data, function(error, equal) {
             expect(error).to.be.null;
             expect(equal).to.be.equal(true);
             done();
@@ -99,11 +97,10 @@ function differTests(getDifferInstance) {
       var img2 = composePath('wide.png');
       var diff = composePath('sizes.png');
 
-      differInstance.createDiff(img1, img2, function(error, base64) {
-        var buffer = new Buffer(base64, 'base64');
+      differInstance.createDiff(img1, img2, function(error, data) {
         expect(error).to.be.null;
 
-        differInstance.isEqual(diff, buffer, function(error, equal) {
+        differInstance.isEqual(diff, data, function(error, equal) {
           expect(error).to.be.null;
           expect(equal).to.be.equal(true);
           done();
