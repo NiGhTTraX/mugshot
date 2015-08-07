441fbf5e2b59b49418efe3882acd071a69363439
Simion Florentin <florentin.simion@hootsuite.com>
2015-08-07 12:11:52 +0300

    looksSame adaptor to extend object properties

--- test/differ-adaptors.js
+++ test/differ-adaptors.js
@@ -3,6 +3,6 @@ var LooksSameAdaptor = require('../lib/adaptors/looks-same.js');
 
 describe('LooksSame Adaptor', function() {
   test(function() {
-    return new LooksSameAdaptor();
+    return LooksSameAdaptor;
   });
 });
