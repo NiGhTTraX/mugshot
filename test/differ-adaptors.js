4206a6beb83604d26ded8f50e47385e4013d161b
Simion Florentin <florentin.simion@hootsuite.com>
2015-08-07 13:41:46 +0300

    use plural

--- test/differ-adaptors.js
+++ test/differ-adaptors.js
@@ -1,8 +1,8 @@
-var test = require('./differ-tests.js');
+var tests = require('./differ-tests.js');
 var LooksSameAdaptor = require('../lib/adaptors/looks-same.js');
 
 describe('LooksSame Adaptor', function() {
-  test(function() {
+  tests(function() {
     return LooksSameAdaptor;
   });
 });
