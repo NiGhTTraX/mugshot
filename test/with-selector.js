b4f6b294527a0910d373d646c90ccaf234d33207
Simion Florentin <florentin.simion@hootsuite.com>
2015-08-19 16:14:18 +0300

    add with selector suite in separate file

---
+++ test/with-selector.js
@@ -0,0 +1,81 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var expect = chai.expect;
+var Mugshot = require('../lib/mugshot.js');
+
+chai.use(require('sinon-chai'));
+
+describe('With selector', function() {
+  var withSelector = {
+        name: 'path',
+        selector: 'a dom selector'
+      },
+      rect = {
+        width: 100,
+        height: 100,
+        top: 50,
+        left: 50
+      },
+      error = new Error('Fatal Error'),
+      screenshot = new Buffer('ZmxvcmVudGlu'),
+      callback, mugshot, browser, PNGProcessor, FS;
+
+  beforeEach(function() {
+    browser = {
+      takeScreenshot: sinon.stub().yields(null, screenshot),
+      getBoundingClientRect: sinon.stub()
+    };
+
+    FS = {
+      mkdir: sinon.stub().yields(null)
+    };
+
+    PNGProcessor = {
+      crop: sinon.stub()
+    };
+
+    callback = sinon.spy();
+
+    var options = {
+      fs: FS,
+      PNGProcessor: PNGProcessor
+    };
+
+    mugshot = new Mugshot(browser, options);
+  });
+
+  it('should get the bounding rect', function() {
+    mugshot.test(withSelector, callback);
+
+    expect(browser.getBoundingClientRect).to.have.been.calledWith(
+      withSelector.selector, sinon.match.func);
+  });
+
+  it('should call the cb with error if the bounding rect couldn\'t be ' +
+     'calculated', function() {
+    browser.getBoundingClientRect.yields(error);
+
+    mugshot.test(withSelector, callback);
+
+    expect(callback).to.have.been.calledWithExactly(error);
+  });
+
+  it('should crop the image', function() {
+    browser.getBoundingClientRect.yields(null, rect);
+
+    mugshot.test(withSelector, callback);
+
+    expect(PNGProcessor.crop).to.have.been.calledWith(screenshot, rect,
+      sinon.match.func);
+  });
+
+  it('should call the cb with error if the screenshot couldn\'t be cropped',
+     function() {
+    browser.getBoundingClientRect.yields(null, rect);
+    PNGProcessor.crop.yields(error);
+
+    mugshot.test(withSelector, callback);
+
+    expect(callback).to.have.been.calledWithExactly(error);
+  });
+});
