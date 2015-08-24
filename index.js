031b139793b8b1ebb4cedeb9b2e765bb35220ff1
Simion Florentin <florentins@gmail.com>
2015-08-24 17:09:26 +0300

    remove adaptor word

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Mugshot = require('./lib/mugshot.js');
 
 Mugshot.adapters = {
-  WebdriverIOAdaptor: require('./lib/adapters/webdriverio.js'),
-  LooksSameAdaptor: require('./lib/adapters/looks-same.js')
+  WebdriverIO: require('./lib/adapters/webdriverio.js'),
+  LooksSame: require('./lib/adapters/looks-same.js')
 };
 
 module.exports = Mugshot;
