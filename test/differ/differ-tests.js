1a5693f1378138bdd0d15d71dbf3c46a7ddb246b
Simion Florentin <florentin.simion@hootsuite.com>
2015-08-27 17:45:15 +0300

    update differ tests to work only with buffers

--- test/differ/differ-tests.js
+++ test/differ/differ-tests.js
@@ -1,12 +1,22 @@
 var expect = require('chai').expect;
+var fs = require('fs');
 var path = require('path');
 
 function composePath(file) {
-  return path.join(__dirname, 'data', file);
+  var ext = '.png';
+  return path.join(__dirname, 'data', file + ext);
 }
 
 function differTests(getDifferInstance) {
-  var differInstance;
+  var differInstance,
+      same = fs.readFileSync(composePath('same')),
+      different = fs.readFileSync(composePath('different')),
+      unnoticeable = fs.readFileSync(composePath('different-unnoticeable')),
+      tall = fs.readFileSync(composePath('tall')),
+      wide = fs.readFileSync(composePath('wide')),
+      strict = fs.readFileSync(composePath('strict')),
+      strictUnnoticeable = fs.readFileSync(composePath('strict-unnoticeable')),
+      sizes = fs.readFileSync(composePath('sizes'));
 
   beforeEach(function() {
     differInstance = getDifferInstance();
@@ -14,46 +24,43 @@ function differTests(getDifferInstance) {
 
   describe('isEqual method', function() {
     it('should return true for exactly same image', function(done) {
-      var img1 = composePath('same.png');
 
-      differInstance.isEqual(img1, img1, function(error, equal) {
+      differInstance.isEqual(same, same, function(error, equal) {
         expect(error).to.be.null;
         expect(equal).to.be.true;
+
         done();
       });
     });
 
     it('should return false for different images', function(done) {
-      var img1 = composePath('same.png');
-      var img2 = composePath('different.png');
 
-      differInstance.isEqual(img1, img2, function(error, equal) {
+      differInstance.isEqual(same, different, function(error, equal) {
         expect(error).to.be.null;
         expect(equal).to.be.false;
+
         done();
       });
     });
 
     it('should also return false if differences are unnoticeable',
       function(done) {
-        var img1 = composePath('same.png');
-        var img2 = composePath('different-unnoticeable.png');
 
-        differInstance.isEqual(img1, img2, function(error, equal) {
+        differInstance.isEqual(same, unnoticeable, function(error, equal) {
           expect(error).to.be.null;
           expect(equal).to.be.false;
+
           done();
         });
       }
     );
 
     it('should return false for images of different sizes', function(done) {
-      var img1 = composePath('wide.png');
-      var img2 = composePath('tall.png');
 
-      differInstance.isEqual(img1, img2, function(error, equal) {
+      differInstance.isEqual(tall, wide, function(error, equal) {
         expect(error).to.be.null;
         expect(equal).to.be.false;
+
         done();
       });
     });
@@ -61,14 +68,11 @@ function differTests(getDifferInstance) {
 
   describe('createDiff method', function() {
     it('should create a diff between 2 different images', function(done) {
-      var img1 = composePath('same.png');
-      var img2 = composePath('different.png');
-      var diff = composePath('strict.png');
 
-      differInstance.createDiff(img1, img2, function(error, data) {
+      differInstance.createDiff(same, different, function(error, data) {
         expect(error).to.be.null;
 
-        differInstance.isEqual(diff, data, function(error, equal) {
+        differInstance.isEqual(strict, data, function(error, equal) {
           expect(error).to.be.null;
           expect(equal).to.be.equal(true);
           done();
@@ -77,34 +81,30 @@ function differTests(getDifferInstance) {
     });
 
     it('should also create a proper diff if the differences are unnoticeable',
-      function(done) {
-        var img1 = composePath('same.png');
-        var img2 = composePath('different-unnoticeable.png');
-        var diff = composePath('strict-unnoticeable.png');
+       function(done) {
 
-        differInstance.createDiff(img1, img2, function(error, data) {
-          expect(error).to.be.null;
-
-          differInstance.isEqual(diff, data, function(error, equal) {
-            expect(error).to.be.null;
-            expect(equal).to.be.equal(true);
-            done();
-          });
-        });
-      }
-    );
-
-    it('should create a diff for images of different sizes', function(done) {
-      var img1 = composePath('tall.png');
-      var img2 = composePath('wide.png');
-      var diff = composePath('sizes.png');
-
-      differInstance.createDiff(img1, img2, function(error, data) {
+      differInstance.createDiff(same, unnoticeable, function(error, data) {
         expect(error).to.be.null;
 
-        differInstance.isEqual(diff, data, function(error, equal) {
+        differInstance.isEqual(strictUnnoticeable, data,
+         function(error, equal) {
           expect(error).to.be.null;
           expect(equal).to.be.equal(true);
+
+          done();
+        });
+      });
+    });
+
+    it('should create a diff for images of different sizes', function(done) {
+
+      differInstance.createDiff(tall, wide, function(error, data) {
+        expect(error).to.be.null;
+
+        differInstance.isEqual(sizes, data, function(error, equal) {
+          expect(error).to.be.null;
+          expect(equal).to.be.equal(true);
+
           done();
         });
       });
