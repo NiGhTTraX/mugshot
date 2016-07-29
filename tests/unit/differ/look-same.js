faced68a690d1692ff6909fd1655a6a2b5d41f0a
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-07-29 16:03:45 +0300

    Change unit test #61

--- tests/unit/differ/look-same.js
+++ tests/unit/differ/look-same.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var LooksSameAdapter = require('../../../lib/adapters/looks-same.js');
+var LooksSameAdapter = require('mugshot-looks-same');
 var fs = require('fs');
 var path = require('path');
 
