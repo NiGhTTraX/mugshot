f48008c3b41cfac0e6828568ec24086fe04073fb
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-07-29 15:45:11 +0300

    Delete unused adapter and interface #61

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ var Mugshot = require('./lib/mugshot.js');
 
 Mugshot.adapters = {
   WebdriverIO: require('./lib/adapters/webdriverio.js'),
-  LooksSame: require('./lib/adapters/looks-same.js')
 };
 
 Mugshot.interfaces = {
   browser: require('./lib/interfaces/browser.js'),
-  differ: require('./lib/interfaces/differ.js'),
   fs: require('./lib/interfaces/fs.js'),
   PNGProcessor: require('./lib/interfaces/png-processor.js')
 };
