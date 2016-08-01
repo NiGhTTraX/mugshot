14e6f853b4f158215aa54ce80d097fbcd41a7b3f
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-08-01 11:08:31 +0300

    Put back browser interface #59

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ Mugshot.adapters = {
 };
 
 Mugshot.interfaces = {
+  browser: require('./lib/interfaces/browser.js'),
   differ: require('./lib/interfaces/differ.js'),
   fs: require('./lib/interfaces/fs.js'),
   PNGProcessor: require('./lib/interfaces/png-processor.js')
