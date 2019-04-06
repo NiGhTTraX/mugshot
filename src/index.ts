d4d1747ff1ea0612ad2b5796b7e2e4f0478d4568
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-04-06 22:39:41 +0200

    Do string comparison with basic Browser and FS interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-export default class Mugshot {
+export interface Result {
+  matches: boolean;
+}
 
+export interface VisualRegressionTester {
+  check: (name: string) => Promise<Result>;
+}
+
+export interface Browser {
+  takeScreenshot: () => Promise<string>;
+}
+
+export interface FileSystem {
+  read: (name: string) => Promise<string>;
+  write: (name: string) => Promise<string>;
+}
+
+export default class Mugshot implements VisualRegressionTester {
+  private browser: Browser;
+
+  private fs: FileSystem;
+
+  constructor(browser: Browser, storage: FileSystem) {
+    this.browser = browser;
+    this.fs = storage;
+  }
+
+  check = async (name: string) => {
+    const screenshot = await this.browser.takeScreenshot();
+    const base = await this.fs.read(name);
+
+    return Promise.resolve({ matches: screenshot === base });
+  };
 }
--- tests/node/specs/mugshot.spec.ts
+++ tests/node/specs/mugshot.spec.ts
@@ -1,9 +1,56 @@
-import { describe, it } from '../suite';
-import Mugshot from '../../../src';
+import { describe, it, expect } from '../suite';
+import Mugshot, { Browser, FileSystem } from '../../../src';
+import { Mock } from 'typemoq';
 
 describe('Mugshot', () => {
-  it('should not blow up', () => {
-    // eslint-disable-next-line no-new
-    new Mugshot();
+  it('should pass for an existing identical screenshot', async () => {
+    const base = 'abc';
+
+    const browser = Mock.ofType<Browser>();
+    browser
+      .setup(b => b.takeScreenshot())
+      .returns(() => Promise.resolve(base))
+      .verifiable();
+
+    const fs = Mock.ofType<FileSystem>();
+    fs
+      .setup(f => f.read('existing-identical'))
+      .returns(() => Promise.resolve(base))
+      .verifiable();
+
+    const mugshot = new Mugshot(browser.object, fs.object);
+
+    const result = await mugshot.check('existing-identical');
+
+    expect(result.matches).to.be.true;
+
+    browser.verifyAll();
+    fs.verifyAll();
+  });
+
+  it('should fail for an existing diff screenshot', async () => {
+    const base = 'abc';
+    const screenshot = 'not-abc';
+
+    const browser = Mock.ofType<Browser>();
+    browser
+      .setup(b => b.takeScreenshot())
+      .returns(() => Promise.resolve(screenshot))
+      .verifiable();
+
+    const fs = Mock.ofType<FileSystem>();
+    fs
+      .setup(f => f.read('existing-diff'))
+      .returns(() => Promise.resolve(base))
+      .verifiable();
+
+    const mugshot = new Mugshot(browser.object, fs.object);
+
+    const result = await mugshot.check('existing-diff');
+
+    expect(result.matches).to.be.false;
+
+    browser.verifyAll();
+    fs.verifyAll();
   });
 });
