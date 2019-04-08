843d09ffc3e351438d17f2ac8a452cff9529d817
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-04-08 16:12:00 +0200

    Reduce duplication in tests

--- tests/node/specs/mugshot.spec.ts
+++ tests/node/specs/mugshot.spec.ts
@@ -5,24 +5,44 @@ import PNGEditor from '../../../src/interfaces/png-editor';
 import { blackPixelB64, blackPixelBuffer, whitePixelBuffer } from '../fixtures';
 
 describe('Mugshot', () => {
-  it('should pass for an existing identical screenshot', async () => {
+  function getFsWithExistingBaseline(path: string, buffer: Buffer) {
+    const fs = Mock.ofType<FileSystem>();
+    fs
+      .setup(f => f.readFile(path))
+      .returns(() => Promise.resolve(buffer))
+      .verifiable();
+    return fs;
+  }
+
+  function getBrowserWithScreenshot(base64: string) {
     const browser = Mock.ofType<Browser>();
     browser
       .setup(b => b.takeScreenshot())
-      .returns(() => Promise.resolve(blackPixelB64))
+      .returns(() => Promise.resolve(base64))
       .verifiable();
+    return browser;
+  }
 
-    const fs = Mock.ofType<FileSystem>();
-    fs
-      .setup(f => f.readFile('results/existing-identical.png'))
-      .returns(() => Promise.resolve(blackPixelBuffer))
-      .verifiable();
-
+  function getDifferWithResult(base: Buffer, screenshot: Buffer, result: boolean) {
     const differ = Mock.ofType<PNGEditor>();
     differ
-      .setup(d => d.compare(blackPixelBuffer, blackPixelBuffer))
-      .returns(() => Promise.resolve(true))
+      .setup(d => d.compare(base, screenshot))
+      .returns(() => Promise.resolve(result))
       .verifiable();
+    return differ;
+  }
+
+  it('should pass for an existing identical screenshot', async () => {
+    const browser = getBrowserWithScreenshot(blackPixelB64);
+    const fs = getFsWithExistingBaseline(
+      'results/existing-identical.png',
+      blackPixelBuffer
+    );
+    const differ = getDifferWithResult(
+      blackPixelBuffer,
+      blackPixelBuffer,
+      true
+    );
 
     const mugshot = new Mugshot(browser.object, 'results', {
       fs: fs.object,
@@ -39,23 +59,16 @@ describe('Mugshot', () => {
   });
 
   it('should fail for an existing diff screenshot', async () => {
-    const browser = Mock.ofType<Browser>();
-    browser
-      .setup(b => b.takeScreenshot())
-      .returns(() => Promise.resolve(blackPixelB64))
-      .verifiable();
-
-    const fs = Mock.ofType<FileSystem>();
-    fs
-      .setup(f => f.readFile('results/existing-diff.png'))
-      .returns(() => Promise.resolve(whitePixelBuffer))
-      .verifiable();
-
-    const differ = Mock.ofType<PNGEditor>();
-    differ
-      .setup(d => d.compare(whitePixelBuffer, blackPixelBuffer))
-      .returns(() => Promise.resolve(false))
-      .verifiable();
+    const browser = getBrowserWithScreenshot(blackPixelB64);
+    const fs = getFsWithExistingBaseline(
+      'results/existing-diff.png',
+      whitePixelBuffer
+    );
+    const differ = getDifferWithResult(
+      whitePixelBuffer,
+      blackPixelBuffer,
+      false
+    );
 
     const mugshot = new Mugshot(browser.object, 'results', {
       fs: fs.object,
