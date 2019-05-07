839d229cb8202a6601e4c4f6a82c681d70bdaa6e
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-05-07 21:20:05 +0200

    Revert "Support taking screenshots of elements with existing baselines"
    This reverts commit 555da8d4. Went a bit ahead of myself.

--- src/interfaces/browser.ts
+++ src/interfaces/browser.ts
@@ -1,13 +1,6 @@
-export type ElementSelector = string;
-
 /* eslint-disable semi */
 export default interface Browser {
   // Take a full page screenshot and return a base64 string.
   // https://w3c.github.io/webdriver/#take-screenshot
   takeScreenshot: () => Promise<string>;
-
-  // Take a screenshot of the visible region encompassed by the bounding
-  // rectangle of an element.
-  // https://w3c.github.io/webdriver/#dfn-take-element-screenshot
-  takeElementScreenshot: (selector: ElementSelector) => Promise<string>;
 }
