0e65e704de60a9a1c5436adf0934d7d0ee48aca9
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-05-14 15:46:31 +0200

    Add GUI test for element screenshots

---
+++ packages/mugshot/tests/gui/specs/element.spec.ts
@@ -0,0 +1,37 @@
+import path from 'path';
+import fs from 'fs-extra';
+import { beforeEach, describe, expect, it, loadFixture } from '../../../../../tests/gui/suite';
+import Mugshot from '../../../src/lib/mugshot';
+import jimpDiffer from '../../../src/lib/jimp-differ';
+import WebdriverIOAdapter from '@mugshot/webdriverio';
+
+describe('Mugshot', () => {
+  describe('Element screenshots', () => {
+    let resultsPath!: string;
+
+    beforeEach(async () => {
+      const browser = process.env.BROWSER;
+
+      resultsPath = await fs.mkdtemp(`/tmp/mugshot-${browser}`);
+
+      // TODO: create a helper for this
+      await fs.copyFile(
+        path.join(__dirname, `../../../../../tests/gui/screenshots/${browser}/rect.png`),
+        path.join(resultsPath, 'rect.png')
+      );
+    });
+
+    it('should take a screenshot of an element', async browser => {
+      await loadFixture('rect');
+
+      const mugshot = new Mugshot(new WebdriverIOAdapter(browser), resultsPath, {
+        fs,
+        pngDiffer: jimpDiffer
+      });
+
+      const result = await mugshot.check('rect', '.test');
+
+      expect(result.matches).to.be.true;
+    });
+  });
+});
