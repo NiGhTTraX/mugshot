0dab6336ee7b9d968d5266093adf347372ac259e
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-05-21 17:44:59 +0200

    Export all interfaces from Mugshot

--- packages/mugshot/src/index.ts
+++ packages/mugshot/src/index.ts
@@ -1,8 +1,14 @@
 import Mugshot from './lib/mugshot';
 import Browser from './interfaces/browser';
+import PNGProcessor from './interfaces/png-processor';
+import PNGDiffer from './interfaces/png-differ';
+import FileSystem from './interfaces/file-system';
 
 export default Mugshot;
 
 export {
-  Browser
+  Browser,
+  PNGDiffer,
+  PNGProcessor,
+  FileSystem
 };
