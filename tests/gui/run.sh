b665741d0475702583999ff6b1a3ad2b6bb7b0b8
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-06-17 00:42:03 +0200

    Rename package

--- tests/gui/run.sh
+++ tests/gui/run.sh
@@ -2,14 +2,14 @@
 
 trap cleanup EXIT
 cleanup() {
-  ../../packages/gui-tests/src/stop.sh
+  ../../packages/selenium/src/stop.sh
 }
 
 set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-../../packages/gui-tests/src/start.sh
+../../packages/selenium/src/start.sh
 
 COVERAGE=1 BROWSER=chrome npm run _test:gui
 COVERAGE=1 BROWSER=firefox npm run _test:gui
