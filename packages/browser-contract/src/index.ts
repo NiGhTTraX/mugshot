3e20e9e93cccc2696276e352ea21fcf6b178c5f5
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-07-10 15:24:10 +0200

    Move @mugshot/selenium index.js to src/ to match other packages

--- packages/browser-contract/src/index.ts
+++ packages/browser-contract/src/index.ts
@@ -40,8 +40,8 @@ function createFixture(html: string) {
 
 async function loadFixture(browser: BrowserToBeAdapted, adapter: Browser, name: string) {
   const fixtureContent = await fs.readFile(
-    // resolve will give us the location of the root index.js.
-    path.join(require.resolve('@mugshot/selenium'), `../fixtures/${name}.html`),
+    // resolve will give us the location of the src/index.js.
+    path.join(require.resolve('@mugshot/selenium'), `../../fixtures/${name}.html`),
     { encoding: 'utf8' }
   );
 
