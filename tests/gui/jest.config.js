90453fe65cf7f88a10ff7ba10dab545099d8076c
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-07-12 11:27:10 +0200

    Increase test timeouts across the board

--- tests/gui/jest.config.js
+++ tests/gui/jest.config.js
@@ -1,7 +1,6 @@
 const baseConfig = require('../jest.config');
 const path = require('path');
 
-// TODO: increase timeouts with https://github.com/facebook/jest/pull/8456
 module.exports = {
   ...baseConfig,
   testMatch: ['**/tests/gui/**/*.spec.ts'],
