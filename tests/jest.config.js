9e50914060d36a4f6836280ba1d70d08e255ff23
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-07-20 09:29:49 +0200

    Check coverage at the end

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -15,5 +15,10 @@ module.exports = {
     ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/packages/' }),
     // For some reason the ts-jest helper doesn't pick this one up.
     '^mugshot/(.*)$': '<rootDir>/packages/mugshot/$1'
-  }
+  },
+
+  // We're doing multiple coverage runs so we don't want to print the table every time
+  // and we want to check thresholds once at the end with nyc.
+  coverageReporters: ['json', 'html'],
+  coverageThreshold: undefined
 };
