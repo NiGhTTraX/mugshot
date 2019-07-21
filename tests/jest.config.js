d394b46c10933ebb5978283769d0e83807d43a4b
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-07-21 13:52:33 +0200

    Remove jest config duplication

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,18 +1,12 @@
 const baseConfig = require('tdd-buffet/config/jest.config');
-const path = require('path');
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('../tsconfig');
-
-const rootDir = path.join(__dirname, '../');
 
 module.exports = {
   ...baseConfig,
 
-  rootDir,
   // For some reason Jest will complain that this is duplicating the packages/mugshot/package.json.
   modulePathIgnorePatterns: ['<rootDir>/package.json'],
   moduleNameMapper: {
-    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/packages/' }),
+    ...baseConfig.moduleNameMapper,
     // For some reason the ts-jest helper doesn't pick this one up.
     '^mugshot/(.*)$': '<rootDir>/packages/mugshot/$1'
   },
