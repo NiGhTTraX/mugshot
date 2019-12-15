30d3beaea317e7989ee03613c061cf1d436f7776
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-12-15 15:27:36 +0100

    Revert "Simplify index exports"
    This reverts commit 9b686a22

--- packages/mugshot/src/index.ts
+++ packages/mugshot/src/index.ts
@@ -1,11 +1,29 @@
-export * from './lib/mugshot';
+import Browser, {
+  ElementNotFoundError,
+  ElementNotVisibleError
+} from './interfaces/browser';
+import PNGDiffer from './interfaces/png-differ';
+import PNGProcessor from './interfaces/png-processor';
+import ScreenshotStorage from './interfaces/screenshot-storage';
+import Screenshotter from './interfaces/screenshotter';
+import FsStorage from './lib/fs-storage';
+import Mugshot from './lib/mugshot';
+import PixelDiffer from './lib/pixel-differ';
+import BrowserScreenshotter from './lib/browser-screenshotter';
+import JimpProcessor from './lib/jimp-processor';
 
-export * from './interfaces/browser';
-export * from './interfaces/png-differ';
-export * from './interfaces/png-processor';
-export * from './interfaces/screenshot-storage';
-export * from './interfaces/screenshotter';
-export * from './lib/fs-storage';
-export * from './lib/pixel-differ';
-export * from './lib/browser-screenshotter';
-export * from './lib/jimp-processor';
+export default Mugshot;
+
+export {
+  Browser,
+  FsStorage,
+  PNGDiffer,
+  PNGProcessor,
+  ScreenshotStorage,
+  Screenshotter,
+  PixelDiffer,
+  ElementNotFoundError,
+  ElementNotVisibleError,
+  BrowserScreenshotter,
+  JimpProcessor
+};
