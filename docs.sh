9868f0a487a5dcae73dd48539828554b067c858e
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-08-29 18:30:19 +0200

    docs: Document docs.sh script

--- docs.sh
+++ docs.sh
@@ -6,4 +6,7 @@ doctoc --title '**Table of content**' README.md DOCS.md
 
 typedoc
 
+# Fix basepath of links: the READMEs reference ./docs from the root
+# but the READMEs get copied to ./docs and that gets deployed.
+# ./docs/foo => ./foo
 sed -i 's/\.\/docs/\./g' docs/index.html
