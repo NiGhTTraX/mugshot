d399280d8e4ca79cb8ab00b7fd440422641f9a2c
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-08-30 21:00:12 +0200

    test(webdriverio): Run tests on Chrome and Firefox from same suite

--- packages/webdriverio/tests/webdriverio-adapter.spec.ts
+++ packages/webdriverio/tests/webdriverio-adapter.spec.ts
@@ -1,36 +1,55 @@
 import { webdriverContractTests } from '@mugshot/contracts';
-import { describe, it } from 'tdd-buffet/suite/gui';
+import { afterEach, beforeEach, describe, it } from 'tdd-buffet/suite/node';
+import { remote, BrowserObject } from 'webdriverio';
 import WebdriverIOAdapter from '../src';
 
 describe('WebdriverIOAdapter', () => {
-  webdriverContractTests.forEach((test) => {
-    it(test.name, (browser) =>
-      test.run(browser, new WebdriverIOAdapter(browser))
-    );
-  });
+  // eslint-disable-next-line no-restricted-syntax
+  for (const browserName of ['chrome', 'firefox']) {
+    describe(browserName, () => {
+      let browser!: BrowserObject;
 
-  // TODO: re-enable these
-  // it('should take a full page screenshot', async (browser) => {
-  //   await loadFixture(browser, 'simple');
-  //
-  //   const clientAdapter = new WebdriverIOAdapter(browser);
-  //   const screenshot = Buffer.from(
-  //     await clientAdapter.takeScreenshot(),
-  //     'base64'
-  //   );
-  //
-  //   await expectIdenticalScreenshots(screenshot, 'simple');
-  // });
-  //
-  // it('should take a full page screenshot with absolutely positioned elements', async (browser) => {
-  //   await loadFixture(browser, 'rect');
-  //
-  //   const clientAdapter = new WebdriverIOAdapter(browser);
-  //   const screenshot = Buffer.from(
-  //     await clientAdapter.takeScreenshot(),
-  //     'base64'
-  //   );
-  //
-  //   await expectIdenticalScreenshots(screenshot, 'full-absolute');
-  // });
+      beforeEach(async () => {
+        const options: WebDriver.Options = {
+          hostname: process.env.SELENIUM_HOST!,
+          port: parseInt(process.env.SELENIUM_PORT!, 10),
+          capabilities: { browserName },
+          logLevel: 'error',
+        };
+
+        browser = await remote(options);
+      });
+
+      afterEach(() => browser.deleteSession());
+
+      webdriverContractTests.forEach((test) => {
+        it(test.name, () => test.run(browser, new WebdriverIOAdapter(browser)));
+      });
+
+      // TODO: re-enable these
+      // it('should take a full page screenshot', async (browser) => {
+      //   await loadFixture(browser, 'simple');
+      //
+      //   const clientAdapter = new WebdriverIOAdapter(browser);
+      //   const screenshot = Buffer.from(
+      //     await clientAdapter.takeScreenshot(),
+      //     'base64'
+      //   );
+      //
+      //   await expectIdenticalScreenshots(screenshot, 'simple');
+      // });
+      //
+      // it('should take a full page screenshot with absolutely positioned elements', async (browser) => {
+      //   await loadFixture(browser, 'rect');
+      //
+      //   const clientAdapter = new WebdriverIOAdapter(browser);
+      //   const screenshot = Buffer.from(
+      //     await clientAdapter.takeScreenshot(),
+      //     'base64'
+      //   );
+      //
+      //   await expectIdenticalScreenshots(screenshot, 'full-absolute');
+      // });
+    });
+  }
 });
