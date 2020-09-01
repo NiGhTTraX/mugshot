ade9bbeab5c05be1d7132272b2e36394620a6f53
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-09-01 20:38:49 +0200

    test(playwright): Add screenshot tests

--- packages/playwright/tests/playwright.spec.ts
+++ packages/playwright/tests/playwright.spec.ts
@@ -1,17 +1,23 @@
-import { webdriverContractTests } from '@mugshot/contracts';
+import {
+  Fixture,
+  loadFixture,
+  webdriverContractTests,
+} from '@mugshot/contracts';
+import { join } from 'path';
 import playwright, { Browser, Page } from 'playwright';
 import { afterEach, beforeEach, describe, it } from 'tdd-buffet/suite/node';
+import { expectIdenticalScreenshots } from '../../../tests/helpers';
 import PlaywrightAdapter from '../src';
 
 describe(`PlaywrightAdapter`, () => {
   // eslint-disable-next-line no-restricted-syntax
-  for (const browserType of ['chromium', 'firefox'] as const) {
-    describe(browserType, () => {
+  for (const browserName of ['chromium', 'firefox'] as const) {
+    describe(browserName, () => {
       let browser!: Browser;
       let page!: Page;
 
       beforeEach(async () => {
-        browser = await playwright[browserType].launch();
+        browser = await playwright[browserName].launch();
         const context = await browser.newContext();
         page = await context.newPage();
       });
@@ -31,7 +37,45 @@ describe(`PlaywrightAdapter`, () => {
         );
       });
 
-      // TODO: add screenshots tests
+      it('should take a full page screenshot', async () => {
+        const clientAdapter = new PlaywrightAdapter(page);
+
+        await loadFixture(
+          { url: (path) => page.goto(path) },
+          clientAdapter,
+          Fixture.simple
+        );
+
+        const screenshot = Buffer.from(
+          await clientAdapter.takeScreenshot(),
+          'base64'
+        );
+
+        await expectIdenticalScreenshots(
+          screenshot,
+          join(__dirname, `screenshots/${browserName}/simple.png`)
+        );
+      });
+
+      it('should take a full page screenshot with absolutely positioned elements', async () => {
+        const clientAdapter = new PlaywrightAdapter(page);
+
+        await loadFixture(
+          { url: (path) => page.goto(path) },
+          clientAdapter,
+          Fixture.rect
+        );
+
+        const screenshot = Buffer.from(
+          await clientAdapter.takeScreenshot(),
+          'base64'
+        );
+
+        await expectIdenticalScreenshots(
+          screenshot,
+          join(__dirname, `screenshots/${browserName}/full-absolute.png`)
+        );
+      });
     });
   }
 });
