c762f2bb60d91916ecd848bb1f4806d1a4379824
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-09-01 20:38:44 +0200

    test(puppeteer): Add screenshot tests

--- packages/puppeteer/tests/puppeteer-adapter.spec.ts
+++ packages/puppeteer/tests/puppeteer-adapter.spec.ts
@@ -1,7 +1,13 @@
-import { webdriverContractTests } from '@mugshot/contracts';
+import {
+  Fixture,
+  loadFixture,
+  webdriverContractTests,
+} from '@mugshot/contracts';
 import PuppeteerAdapter from '@mugshot/puppeteer';
+import { join } from 'path';
 import puppeteer from 'puppeteer';
 import { afterEach, beforeEach, describe, it } from 'tdd-buffet/suite/node';
+import { expectIdenticalScreenshots } from '../../../tests/helpers';
 
 describe('PuppeteerAdapter', () => {
   let browser!: puppeteer.Browser, page!: puppeteer.Page;
@@ -26,5 +32,43 @@ describe('PuppeteerAdapter', () => {
     );
   });
 
-  // TODO: add screenshots tests
+  it('should take a full page screenshot', async () => {
+    const clientAdapter = new PuppeteerAdapter(page);
+
+    await loadFixture(
+      { url: (path) => page.goto(path) },
+      clientAdapter,
+      Fixture.simple
+    );
+
+    const screenshot = Buffer.from(
+      await clientAdapter.takeScreenshot(),
+      'base64'
+    );
+
+    await expectIdenticalScreenshots(
+      screenshot,
+      join(__dirname, `screenshots/simple.png`)
+    );
+  });
+
+  it('should take a full page screenshot with absolutely positioned elements', async () => {
+    const clientAdapter = new PuppeteerAdapter(page);
+
+    await loadFixture(
+      { url: (path) => page.goto(path) },
+      clientAdapter,
+      Fixture.rect
+    );
+
+    const screenshot = Buffer.from(
+      await clientAdapter.takeScreenshot(),
+      'base64'
+    );
+
+    await expectIdenticalScreenshots(
+      screenshot,
+      join(__dirname, `screenshots/full-absolute.png`)
+    );
+  });
 });
