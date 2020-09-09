55e9eabb4153e36d1c2f7657d5c91883511dfe84
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-09-09 19:56:43 +0200

    test: Run all tests from the root

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -10,9 +10,4 @@ module.exports = {
     // For some reason the ts-jest helper doesn't pick this one up.
     '^mugshot/(.*)$': '<rootDir>/packages/mugshot/$1',
   },
-
-  // We're doing multiple coverage runs so we don't want to print the table every time
-  // and we want to check thresholds once at the end with nyc.
-  coverageReporters: ['json', 'html'],
-  coverageThreshold: undefined,
 };
