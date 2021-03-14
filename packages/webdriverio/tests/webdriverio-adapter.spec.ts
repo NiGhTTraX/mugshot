baf0fc9da865d00919b100c7558da695da7dbaeb
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2021-03-14 10:46:57 +0100

    build(webdriverio): Use matrix for browsers

--- packages/webdriverio/tests/webdriverio-adapter.spec.ts
+++ packages/webdriverio/tests/webdriverio-adapter.spec.ts
@@ -3,35 +3,38 @@ import { after, before, describe, it } from 'tdd-buffet/suite/node';
 import { Browser, remote, RemoteOptions } from 'webdriverio';
 import WebdriverIOAdapter from '../src';
 
+const { BROWSER } = process.env;
+
+if (!BROWSER) {
+  throw new Error('BROWSER env var missing');
+}
+
 describe('WebdriverIOAdapter', () => {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const browserName of ['chrome', 'firefox']) {
-    describe(browserName, () => {
-      let browser!: Browser<'async'>;
+  describe(BROWSER, () => {
+    let browser!: Browser<'async'>;
 
-      before(async () => {
-        const options: RemoteOptions = {
-          hostname: process.env.SELENIUM_HOST || 'localhost',
-          port: parseInt(process.env.SELENIUM_PORT || '4444', 10),
-          path: '/wd/hub',
-          capabilities: { browserName },
-          logLevel: 'error',
-        };
+    before(async () => {
+      const options: RemoteOptions = {
+        hostname: process.env.SELENIUM_HOST || 'localhost',
+        port: parseInt(process.env.SELENIUM_PORT || '4444', 10),
+        path: '/wd/hub',
+        capabilities: { browserName: BROWSER },
+        logLevel: 'error',
+      };
 
-        browser = await remote(options);
-      });
+      browser = await remote(options);
+    });
 
-      after(() => browser.deleteSession());
+    after(() => browser.deleteSession());
 
-      Object.keys(webdriverContractSuites).forEach((suite) => {
-        describe(suite, () => {
-          webdriverContractSuites[suite].forEach((test) => {
-            it(test.name, () =>
-              test.run(browser, new WebdriverIOAdapter(browser))
-            );
-          });
+    Object.keys(webdriverContractSuites).forEach((suite) => {
+      describe(suite, () => {
+        webdriverContractSuites[suite].forEach((test) => {
+          it(test.name, () =>
+            test.run(browser, new WebdriverIOAdapter(browser))
+          );
         });
       });
     });
-  }
+  });
 });
