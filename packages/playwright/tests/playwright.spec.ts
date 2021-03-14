33b83bbfc6f69b7a364db52ec775ef5ec1b015b7
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2021-03-14 10:56:07 +0100

    build(playwright): Use matrix for browsers

--- packages/playwright/tests/playwright.spec.ts
+++ packages/playwright/tests/playwright.spec.ts
@@ -3,38 +3,43 @@ import playwright, { Browser, BrowserContext, Page } from 'playwright';
 import { after, before, beforeEach, describe, it } from 'tdd-buffet/suite/node';
 import PlaywrightAdapter from '../src';
 
+const { BROWSER } = process.env as {
+  BROWSER: 'chromium' | 'firefox' | undefined;
+};
+
+if (!BROWSER) {
+  throw new Error('BROWSER env var missing');
+}
+
 describe(`PlaywrightAdapter`, () => {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const browserName of ['chromium', 'firefox'] as const) {
-    describe(browserName, () => {
-      let browser!: Browser;
-      let page!: Page;
-      let context!: BrowserContext;
+  describe(BROWSER, () => {
+    let browser!: Browser;
+    let page!: Page;
+    let context!: BrowserContext;
 
-      before(async () => {
-        browser = await playwright[browserName].launch();
-        context = await browser.newContext();
-      });
+    before(async () => {
+      browser = await playwright[BROWSER].launch();
+      context = await browser.newContext();
+    });
 
-      beforeEach(async () => {
-        page = await context.newPage();
-      });
+    beforeEach(async () => {
+      page = await context.newPage();
+    });
 
-      after(async () => {
-        await browser.close();
-      });
+    after(async () => {
+      await browser.close();
+    });
 
-      const setup = {
-        url: (path: string) => page.goto(path),
-      };
+    const setup = {
+      url: (path: string) => page.goto(path),
+    };
 
-      Object.keys(webdriverContractSuites).forEach((suite) => {
-        describe(suite, () => {
-          webdriverContractSuites[suite].forEach((test) => {
-            it(test.name, () => test.run(setup, new PlaywrightAdapter(page)));
-          });
+    Object.keys(webdriverContractSuites).forEach((suite) => {
+      describe(suite, () => {
+        webdriverContractSuites[suite].forEach((test) => {
+          it(test.name, () => test.run(setup, new PlaywrightAdapter(page)));
         });
       });
     });
-  }
+  });
 });
