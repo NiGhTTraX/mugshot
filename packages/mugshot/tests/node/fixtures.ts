b85b0684848c99ea669a2fde5a2232518e6c7eda
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2022-11-12 21:53:10 +0100

    chore: Fix lint errors

--- packages/mugshot/tests/node/fixtures.ts
+++ packages/mugshot/tests/node/fixtures.ts
@@ -1,11 +1,14 @@
 // Generated with http://png-pixel.com/.
 export const blackPixelB64 =
   'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=';
+
 export const blackPixelBuffer = Buffer.from(blackPixelB64, 'base64');
+
 export const whitePixelBuffer = Buffer.from(
   'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sA AAAASUVORK5CYII=',
   'base64'
 );
+
 export const redPixelBuffer = Buffer.from(
   'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4AWP4z8DwHwAFAAH/e+m+ 7wAAAABJRU5ErkJggg==',
   'base64'
